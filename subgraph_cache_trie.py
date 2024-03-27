ef81147266b19fc554bc5954554f147f3aa23ec3
Matt Weiden <matthew.weiden@gmail.com>
2024-03-26 22:00:20 -0700

    Performant way of getting sorted cached keys

--- subgraph_cache_trie.py
+++ subgraph_cache_trie.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass, field
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 from trie import Node, Trie
 
@@ -7,6 +7,28 @@ from trie import Node, Trie
 @dataclass
 class SubgraphCacheNode(Node):
     cache: Dict[str, Any] = field(default_factory=dict)
+    is_dirty: bool = field(default=False)
+    _cache_sorted_keys: List[str] = field(default_factory=list)
+
+    @property
+    def cache_sorted_keys(self):
+        """cache_sorted_keys
+
+        For faster access, we only want to sort the keys in the cache once per
+        unique combination of keys. We do this by maintaining whether there have
+        been insertions or deletions in the `is_dirty` state. We can skip
+        sorting the list when no writes have been performed.
+        """
+        if self.is_dirty:
+            self._cache_sorted_keys = [
+                ele[0] for ele in sorted(
+                    self.cache.items(),
+                    key=lambda kv: kv[1],
+                    reverse=True,
+                )
+            ]
+            self.is_dirty = False
+        return self._cache_sorted_keys
 
 
 class SubgraphCacheTrie(Trie):
@@ -17,16 +39,21 @@ class SubgraphCacheTrie(Trie):
     def insert(self, key, value):
         node = super().insert(key, value)
         node.cache[node.key] = node.value
+        node.is_dirty = True
         for ancestor in self._ancestors(node):
             ancestor.cache[node.key] = node.value
+            ancestor.is_dirty = True
 
     def delete(self, key):
         node = self.find(key)
         if node is None:
             return
         super().delete(key)
+        del node.cache[node.key]
+        node.is_dirty = True
         for ancestor in self._ancestors(node):
             del ancestor.cache[node.key]
+            ancestor.is_dirty = True
 
     @staticmethod
     def _ancestors(node: SubgraphCacheNode):
--- tests/test_subgraph_cache_trie.py
+++ tests/test_subgraph_cache_trie.py
@@ -11,6 +11,11 @@ def test_insert():
     assert t.root["a"]["l"].cache == {"all": 1}
     assert t.root["a"]["n"].cache == {"any": 2, "an": 3}
 
+    assert t.root.cache_sorted_keys == ["an", "any", "all"]
+    assert t.root["a"].cache_sorted_keys == ["an", "any", "all"]
+    assert t.root["a"]["l"].cache_sorted_keys == ["all"]
+    assert t.root["a"]["n"].cache_sorted_keys == ["an", "any"]
+
 def test_delete():
     t = SubgraphCacheTrie()
     t.insert("all", 1)
@@ -18,6 +23,11 @@ def test_delete():
     t.insert("an", 3)
     t.delete("all")
     assert t.find("all") is None
+
     assert t.root.cache == {"any": 2, "an": 3}
     assert t.root["a"].cache == {"any": 2, "an": 3}
     assert t.root["a"]["n"].cache == {"any": 2, "an": 3}
+
+    assert t.root.cache_sorted_keys == ["an", "any"]
+    assert t.root["a"].cache_sorted_keys == ["an", "any"]
+    assert t.root["a"]["n"].cache_sorted_keys == ["an", "any"]
